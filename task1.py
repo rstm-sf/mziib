7bc211149fc483566e74b6f1cd0c612cbd23af47
rstm-sf <Rustam Sayfutdinov>
2017-09-22 01:07:00 +0300

    autopep8...

--- task1.py
+++ task1.py
@@ -11,17 +11,17 @@ def sub_of_GF(a, b, p):
 
 
 def xgcd(a, b):
-	'''
-	Iterative Extended Euclidean algorithm
-	'''
+    '''
+    Iterative Extended Euclidean algorithm
+    '''
     x0, x1, y0, y1 = 1, 0, 0, 1
-    
+
     while b != 0:
         q, a, b = a // b, b, a % b
         x0, x1 = x1, x0 - q * x1
         y0, y1 = y1, y0 - q * y1
-        
-    return  a, x0, y0
+
+    return a, x0, y0
 
 
 def mul_inv_of_GF(a, p):
@@ -38,26 +38,26 @@ def div_of_GF(a, b, p):
 def find_ord_GF(q, p):
     if p == 1 or p < q or p % q == 0:
         return -1
-    
-    t      = q % p
+
+    t = q % p
     my_ord = 1
-    
+
     while t != 1:
         t = mul_of_GF(t, q, p)
         my_ord += 1
-        
+
     return my_ord
 
 
 def task_find_ord():
     print('Введите числа: q - любое число конечного поля F(p), p - порядок поля.')
-    
-    print('p = ', end = '')
+
+    print('p = ', end='')
     p = int(input())
     if p < 2:
         print('Неверное число!')
-        
-    print('q = ', end = '')
+
+    print('q = ', end='')
     q = int(input())
     if q < 0:
         print('Неверное число!')
